a924fcb4d916a93d274ab44ad2173c6296caea7a
Oroni Hasan <o3hasan@uwaterloo.ca>
2026-04-10 00:14:09 -0400

    translation working

--- commands/utility/translate.js
+++ commands/utility/translate.js
@@ -1,22 +1,65 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { Translate } = require('@google-cloud/translate').v2;
+
+const translate = new Translate();
+
+let cachedLanguages = null;
+
+async function getLanguages() {
+	if (!cachedLanguages) {
+		const [languages] = await translate.getLanguages();
+		cachedLanguages = languages;
+	}
+	return cachedLanguages;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('translate')
 		.setDescription('Translates a message to English')
-		.addStringOption((option) => option.setName('message_link').setDescription('Enter link of message').setRequired(true)),
+		.addStringOption((option) => option
+			.setName('message_link')
+			.setDescription('Enter link of message')
+			.setRequired(true))
+		.addStringOption((option) => option
+			.setName('translate_to')
+			.setDescription('Pick the language you\'d like to translate to')
+			.setAutocomplete(true)),
+
+	async autocomplete(interaction) {
+		const focusedValue = interacgition.options.getFocused().toLowerCase();
+
+		const languages = await getLanguages();
+
+		const filtered = languages
+			.filter((choice) => {
+				const query = focusedValue;
+				return choice.name.toLowerCase().startsWith(query)
+				|| choice.code.toLowerCase().startsWith(query);
+			})
+			.slice(0, 25);
+
+		await interaction.respond(filtered.map((choice) => ({ name: choice.name, value: choice.code })));
+	},
 	
 	async execute(interaction) {
+		await interaction.deferReply(); // delay response since waiting for fetches takes longer than default 3 second timer for responding
+
 		const messageLink = interaction.options.getString('message_link');
 		const ids = messageLink.slice('https://discord.com/channels/'.length).split('/');
 		const [, channelId, messageId] = ids;
 
 		const channel = await interaction.guild.channels.fetch(channelId);
-		console.log(channel);
-
 		const message = await channel.messages.fetch(messageId);
-		console.log(message);
 
-		await interaction.reply(`${message.content}`);
+		const text = message.content;
+		const target = interaction.options.getString('translate_to') || 'en';
+
+		let [ translations ] = await translate.translate(text, target);
+		translations = Array.isArray(translations) ? translations : [translations];
+
+		console.log(`translation: ${translations}`);
+
+		await interaction.editReply(`Message by ${message.author.globalName}: ${text}\n Translation to ${target}: ${translations}`);
 	},
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,8 +3,6 @@ const { Events, MessageFlags } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
-
 		const command = interaction.client.commands.get(interaction.commandName);
 
 		if (!command) {
@@ -12,20 +10,28 @@ module.exports = {
 			return;
 		}
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({
-					content: 'There was an error while executing this command! Replied or Deferred',
-					flags: MessageFlags.Ephemeral,
-				});
-			} else {
-				await interaction.reply({
-					content: 'There was an error while executing this command! Reply',
-					flags: MessageFlags.Ephemeral,
-				});
+		if (interaction.isAutocomplete()) {
+			try {
+				await command.autocomplete(interaction);
+			} catch (error) {
+				console.error(error);
+			}
+		} else if (interaction.isChatInputCommand()) {
+			try {
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(error);
+				if (interaction.replied || interaction.deferred) {
+					await interaction.followUp({
+						content: 'There was an error while executing this command! Replied or Deferred',
+						flags: MessageFlags.Ephemeral,
+					});
+				} else {
+					await interaction.reply({
+						content: 'There was an error while executing this command! Reply',
+						flags: MessageFlags.Ephemeral,
+					});
+				}
 			}
 		}
 	},
